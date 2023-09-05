6996aada58a27b59709416c831c7263a765b31b0
kritan10 <kritant37@gmail.com>
2023-09-05 15:07:51 +0545

    implemented auth interceptor

--- app/src/main/java/com/kritan/nityahealth/base/di/NetworkModule.kt
+++ app/src/main/java/com/kritan/nityahealth/base/di/NetworkModule.kt
@@ -1,9 +1,12 @@
 package com.kritan.nityahealth.base.di
 
+import com.kritan.nityahealth.auth.AppAuth
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,10 +18,28 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofitService(): Retrofit {
+    fun provideRetrofitService(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesAuthClient(appAuth: AppAuth): OkHttpClient {
+        val authToken = appAuth.authState.value.token ?: ""
+        val interceptor = Interceptor { chain ->
+            val request = chain.request()
+            val authRequest = request.newBuilder()
+                .addHeader("Accept", "application/json")
+                .addHeader("Authorization", "Bearer $authToken")
+                .build()
+            chain.proceed(authRequest)
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
             .build()
     }
 }

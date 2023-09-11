0dd4766499b4ed0adc99092e4cbd3485edd92d09
kritan10 <kritant37@gmail.com>
2023-09-11 12:16:50 +0545

    manually set material3 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,11 +78,10 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.1")
 
     //Icons-Extended
     implementation("androidx.compose.material:material-icons-extended")
-
 
     //Accompanist
     implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanist_version")

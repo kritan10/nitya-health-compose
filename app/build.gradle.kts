fae903a1f6540ebcba0ccdcecd68069c3197ab9c
kritan10 <kritant37@gmail.com>
2023-09-12 12:19:48 +0545

    bumped dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ android {
 
 
 dependencies {
-    val nav_version = "2.7.0"
+    val nav_version = "2.7.2"
     val accompanist_version = "0.30.1"
     val retrofit_version = "2.9.0"
     val hilt_version = "2.47"
@@ -78,9 +78,11 @@ dependencies {
     //ksp
     implementation("com.google.devtools.ksp:symbol-processing-api:1.9.0-1.0.11")
 
-    //Core
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    //Kotlin extensions
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    //Compose
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")

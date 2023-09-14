dfa08c84b409eaede1a268e607ba303297da012d
Kritan Thapa <kritant37@gmail.com>
2023-09-14 20:39:00 +0545

    added comments

--- app/src/main/java/com/kritan/nityahealth/auth/data/api/AuthApi.kt
+++ app/src/main/java/com/kritan/nityahealth/auth/data/api/AuthApi.kt
@@ -8,8 +8,21 @@ import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 
-
+/**
+ * Retrofit interface for authentication. It is used for logging in and registration.
+ */
 interface AuthApi {
+    /**
+     * POST request that sends the email and password to the server and receives an [AuthToken].
+     * The request data is sent in a multipart/form-data form.
+     *
+     * @param email the email address of the registered user
+     * @param password the password of the registered user
+     *
+     * @return [AuthToken], an authentication token containing user data and an auth token.
+     * This token can be used to send request to authenticated routes.
+     * The response is wrapped in a generic [ApiResponse] which is again wrapped in [retrofit2.Response]
+     */
     @Multipart
     @POST("login")
     suspend fun login(
@@ -17,6 +30,22 @@ interface AuthApi {
         @Part password: MultipartBody.Part
     ): Response<ApiResponse<AuthToken>>
 
+
+    /**
+     * POST request that sends the user data for registration to the server and receives an [AuthToken].
+     * The request data is sent in a multipart/form-data form.
+     *
+     * @param name name of the user
+     * @param email email address of the user
+     * @param address physical address of the user
+     * @param phone phone number of the user
+     * @param password new password of the user
+     * @param confirmPassword confirmation password, must be same as [password]
+     *
+     * @return [AuthToken], an authentication token containing user data and an auth token.
+     * This token can be used to send request to authenticated routes.
+     * The response is wrapped in a generic [ApiResponse] which is again wrapped in [retrofit2.Response]
+     */
     @Multipart
     @POST("register")
     suspend fun register(

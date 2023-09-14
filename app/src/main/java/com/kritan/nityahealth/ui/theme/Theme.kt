46747cc9723d543c3862add81523f6f1759f71ae
Kritan Thapa <kritant37@gmail.com>
2023-09-14 20:40:34 +0545

    disabled dynamic color scheme

--- app/src/main/java/com/kritan/nityahealth/ui/theme/Theme.kt
+++ app/src/main/java/com/kritan/nityahealth/ui/theme/Theme.kt
@@ -18,7 +18,7 @@ import androidx.core.view.WindowCompat
 fun NityaHealthTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {

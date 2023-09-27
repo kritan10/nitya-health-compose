a2271e7f5e7dd8b736927f7d84e3f679a9a5c685
kritan10 <kritant37@gmail.com>
2023-09-27 15:24:46 +0545

    fixed tab-pager animation and sync issues

--- app/src/main/java/com/kritan/nityahealth/feature_wellness/presentation/WellnessScreen.kt
+++ app/src/main/java/com/kritan/nityahealth/feature_wellness/presentation/WellnessScreen.kt
@@ -6,12 +6,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -22,10 +21,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kritan.nityahealth.ui.constants.MyPadding
 import com.kritan.nityahealth.ui.layouts.MyGridLayout
 import com.kritan.nityahealth.ui.layouts.MyScaffoldLayout
 import com.kritan.nityahealth.ui.theme.mRoundedCorner
@@ -39,80 +44,83 @@ fun WellnessScreen(
     wellnessViewModel: WellnessViewModel = viewModel()
 ) {
     val wellnessTabItemsList = wellnessViewModel.wellnessTabItemsList
-    val lazyListState = rememberLazyListState()
+
+    var currentTab by remember {
+        mutableIntStateOf(initialPage)
+    }
+
+    val tabRowState = rememberLazyListState()
     val pagerState = rememberPagerState(
         initialPage = initialPage,
         pageCount = { wellnessTabItemsList.size }
     )
 
-    LaunchedEffect(Unit) {
-        wellnessViewModel.uiEvent.collect { event ->
-            when (event) {
-                is UiEvent.ScrollTo -> {
-                    lazyListState.animateScrollToItem(
-                        event.index,
-                        if (event.index > lazyListState.firstVisibleItemIndex) 200 else -100
-                    )
-                    pagerState.scrollToPage(event.index)
-                }
-            }
-        }
+    fun onTabChange(newTabIndex: Int) {
+        currentTab = newTabIndex
+    }
+
+    LaunchedEffect(pagerState.currentPage) {
+        tabRowState.animateScrollToItem(pagerState.currentPage)
+        currentTab = pagerState.currentPage
+    }
+
+    LaunchedEffect(currentTab) {
+        pagerState.animateScrollToPage(currentTab)
     }
 
     MyScaffoldLayout(
-        title = "Wellness", navigateUp = navigateUp,
-        content = {
-            Column {
-                LazyRow(
-                    state = lazyListState,
-                    modifier = Modifier
-                        .padding(vertical = 10.dp),
-                    horizontalArrangement = Arrangement.spacedBy(20.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    item {
-                        Spacer(modifier = Modifier.width(1.dp))
-                    }
-                    items(wellnessTabItemsList) {
-                        RowItem(
-                            tab = it,
-                            tabIndex = it.position,
-                            currentTabIndex = pagerState.currentPage,
-                            onTabChange = wellnessViewModel::onTabChange
-                        )
-                    }
-                    item {
-                        Spacer(modifier = Modifier.width(1.dp))
-                    }
+        title = "Wellness",
+        navigateUp = navigateUp,
+        padding = MyPadding.TopOnly
+    ) {
+        Column {
+            LazyRow(
+                state = tabRowState,
+                modifier = Modifier
+                    .padding(vertical = 10.dp),
+                horizontalArrangement = Arrangement.spacedBy(20.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // initial empty spacing
+                item {}
+
+                items(wellnessTabItemsList) {
+                    TabRowItem(
+                        tab = it,
+                        currentTabIndex = currentTab,
+                        onTabChange = ::onTabChange
+                    )
                 }
 
-                HorizontalPager(
-                    state = pagerState, modifier = Modifier
-                        .fillMaxSize()
-                        .wrapContentSize(
-                            Alignment.TopCenter
-                        )
-                ) {
-                    PagerItem(item = wellnessTabItemsList[it])
-                }
+                // final empty spacing
+                item {}
+            }
+
+            HorizontalPager(
+                state = pagerState, modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(
+                        Alignment.TopCenter
+                    )
+            ) {
+                PagerItem(item = wellnessTabItemsList[it])
             }
         }
-    )
+    }
 }
 
 
 @Composable
-private fun RowItem(
+private fun TabRowItem(
     tab: WellnessTab,
-    tabIndex: Int,
     currentTabIndex: Int,
     onTabChange: (Int) -> Unit
 ) {
-    val isActive = tabIndex == currentTabIndex
+    val isActive = tab.position == currentTabIndex
     var modifier = Modifier
         .border(1.dp, MaterialTheme.colorScheme.primary, mRoundedCorner)
         .clickable {
-            onTabChange(tabIndex)
+            onTabChange(tab.position)
             Log.d("Button", "Clicked")
         }
     if (isActive) {
@@ -129,16 +137,13 @@ private fun RowItem(
 
 @Composable
 private fun PagerItem(item: WellnessTab) {
-    Box(
-        Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.TopCenter)
-    ) {
-        when (item) {
-            WellnessTab.FitnessTab -> MyGridLayout()
-            WellnessTab.FoodTab -> MyGridLayout()
-            WellnessTab.HealthTopicsTab -> MyGridLayout()
-            WellnessTab.PersonalCareTab -> MyGridLayout()
-        }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            item.title,
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Start,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        MyGridLayout()
     }
 }
--- app/src/main/java/com/kritan/nityahealth/feature_wellness/presentation/WellnessViewModel.kt
+++ app/src/main/java/com/kritan/nityahealth/feature_wellness/presentation/WellnessViewModel.kt
@@ -1,12 +1,6 @@
 package com.kritan.nityahealth.feature_wellness.presentation
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.launch
-
 
 class WellnessViewModel : ViewModel() {
     val wellnessTabItemsList = listOf(
@@ -15,25 +9,11 @@ class WellnessViewModel : ViewModel() {
         WellnessTab.PersonalCareTab,
         WellnessTab.HealthTopicsTab,
     )
-
-    private val _uiEvent = Channel<UiEvent> { }
-    val uiEvent = _uiEvent.receiveAsFlow()
-
-    fun onTabChange(newTabIndex: Int) {
-        viewModelScope.launch(Dispatchers.Main) {
-            _uiEvent.send(UiEvent.ScrollTo(newTabIndex))
-        }
-    }
 }
-
 
 sealed class WellnessTab(val title: String, val position: Int) {
     object FitnessTab : WellnessTab("Fitness", 0)
-    object FoodTab : WellnessTab("com.kritan.nityahealth.feature_food.data.models.Food", 1)
+    object FoodTab : WellnessTab("Food", 1)
     object PersonalCareTab : WellnessTab("Personal Care", 2)
     object HealthTopicsTab : WellnessTab("Health Topics", 3)
-}
-
-sealed class UiEvent {
-    data class ScrollTo(val index: Int) : UiEvent()
 }
